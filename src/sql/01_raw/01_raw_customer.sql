618d5d2e276190a08ebbad6c87269c410126006b
jess.manuel@ftwfoundation.org <jess.manuel@ftwfoundation.org>
2026-08-24 15:31:10 +0000

    Creation of Raw Customer Tables
    - Created raw customer tables
    -Validated the source raw count

--- src/sql/01_raw/01_raw_customer.sql
+++ src/sql/01_raw/01_raw_customer.sql
@@ -1,2 +1,23 @@
--- File reserved for the assigned project task.
--- SQL will be added after source inspection is complete.
+-- Creation of Raw Customer Table
+
+CREATE OR REPLACE TABLE workspace.d3_raw.customer_raw AS
+SELECT *
+FROM read_files(
+    '/Volumes/workspace/bronze/1st_volume/shared/week05/chinook_csv/Customer.csv',
+    format => 'csv',
+    header => true,
+    inferSchema => true
+);
+
+-- Validation of Source Raw Count
+SELECT 'Customer' AS source_file, COUNT(*) AS row_count
+FROM read_files(
+    '/Volumes/workspace/bronze/1st_volume/shared/week05/chinook_csv/Customer.csv',
+    format => 'csv',
+    header => true,
+    inferSchema => true
+);
+
+--Viewing the Raw Customer Table
+SELECT *
+FROM workspace.d3_raw.customer_raw;
