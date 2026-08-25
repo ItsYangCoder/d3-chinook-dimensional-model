5ec1b675ca9dfe324ba2e2837471876f37f76bde
jessy <jess.manuel@ftwfoundation.org>
2026-08-26 04:48:11 +0800

    feat: add Customer dimension
    - Create one reporting-ready row per customer in dim_customer
    - Only essential values that are beneficial in answering the business questions are retained to ensure that only relevant elements will be analyzed in the final layer

--- src/sql/03_mart/01_dim_customer.sql
+++ src/sql/03_mart/01_dim_customer.sql
@@ -1,2 +1,22 @@
--- File reserved for the assigned project task.
--- SQL will be added after source inspection is complete.
+-- Create one reporting-ready row per customer in dim_customer
+
+CREATE OR REPLACE TABLE workspace.d3_mart.dim_customer AS
+SELECT
+    -- Business Keys & Attributes
+    customer_id,
+    first_name,
+    last_name,
+    full_name,
+    email_address,
+    phone_number,
+    address,
+    city,
+    country,
+    postal_code,
+    support_rep_id,
+
+    -- Mart Audit Column
+    CURRENT_TIMESTAMP() AS mart_created_at
+FROM workspace.d3_clean.customer_clean;
+
+SELECT * FROM workspace.d3_mart.dim_customer;
