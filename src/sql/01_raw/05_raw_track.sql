aaed5ba53d5be32f0b89f1934388d0974bfda323
joan.maquinano@ftwfoundation.org <joan.maquinano@ftwfoundation.org>
2026-08-27 02:02:25 +0000

    Deleted raw .sql files 5 to 11. All associated tables are now loaded under 04_raw_music (tables: tracks, album, artist, genre, media_type, playlist_track, raw_playlist)

--- src/sql/01_raw/05_raw_track.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TABLE raw_track AS
-SELECT *
-FROM read_files(
-'/Volumes/workspace/bronze/1st_volume/shared/week05/chinook_csv/Track.csv',
-format => 'csv',
-header => true);
-
-SELECT COUNT(*) FROM raw_track;
--- src/sql/01_raw/11_raw_playlist.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE TABLE raw_playlist AS
-SELECT *
-FROM read_files(
-'/Volumes/workspace/bronze/1st_volume/shared/week05/chinook_csv/Playlist.csv',
-format => 'csv',
-header => true);
